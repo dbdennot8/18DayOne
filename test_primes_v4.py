198ad74faa0c3f13bb1f5a9d7d8e61f5759f017e
dbdennot8 <dennisbmithamo@gmail.com>
2017-05-09 00:48:16 +0300

    Version 4 of test cases code

---
+++ test_primes_v4.py
@@ -0,0 +1,21 @@
+import unittest
+
+from prime_numbers import primes
+
+class primes(unittest.TestCase):
+
+	def test_it_flags_negative_input(self):
+		self.assertEqual(primes(-10), "Negative input")
+
+	def test_it_only_takes_integer_inputs(self):
+		self.assertEqual(primes([1,2,3]), "Invalid input")
+
+	def test_it_returns_a_list(self):
+		self.assertTrue(primes(10), [2,3,5,7])
+
+	def test_it_returns_empty_list_when_input_is_zero(self):
+		self.assertEqual(primes(0), [])
+
+if __name__ == '__main__':
+	unittest.main()
+
